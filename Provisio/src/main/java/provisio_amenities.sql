ecd2c55d9384bbff992efe6bdf91b39ef8c61621
cheher21 <cheher@my365.bellevue.edu>
2022-08-25 17:38:40 -0500

    Create Amenities SQL
    Created 36 Rooms with Amenities in SQL

---
+++ Provisio/src/main/java/provisio_amenities.sql
@@ -0,0 +1,144 @@
+-- Room 1
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('1', 'WI-FI', '12.99', '0', '1');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('2', 'Breakfast', '8.99', '0', '1');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('3', 'Parking', '19.99', '0', '1');
+-- Room 2
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('4', 'WI-FI', '12.99', '0', '2');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('5', 'Breakfast', '8.99', '0', '2');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('6', 'Parking', '19.99', '0', '2');
+-- Room 3
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('7', 'WI-FI', '12.99', '0', '3');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('8', 'Breakfast', '8.99', '0', '3');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('9', 'Parking', '19.99', '0', '3');
+-- Room 4
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('10', 'WI-FI', '12.99', '0', '4');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('11', 'Breakfast', '8.99', '0', '4');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('12', 'Parking', '19.99', '0', '4');
+-- Room 5
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('13', 'WI-FI', '12.99', '0', '5');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('14', 'Breakfast', '8.99', '0', '5');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('15', 'Parking', '19.99', '0', '5');
+-- Room 6
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('16', 'WI-FI', '12.99', '0', '6');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('17', 'Breakfast', '8.99', '0', '6');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('18', 'Parking', '19.99', '0', '6');
+-- Room 7
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('19', 'WI-FI', '12.99', '0', '7');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('20', 'Breakfast', '8.99', '0', '7');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('21', 'Parking', '19.99', '0', '7');
+-- Room 8
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('22', 'WI-FI', '12.99', '0', '8');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('23', 'Breakfast', '8.99', '0', '8');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('24', 'Parking', '19.99', '0', '8');
+-- Room 9
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('25', 'WI-FI', '12.99', '0', '9');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('26', 'Breakfast', '8.99', '0', '9');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('27', 'Parking', '19.99', '0', '9');
+-- Room 10
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('28', 'WI-FI', '12.99', '0', '10');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('29', 'Breakfast', '8.99', '0', '10');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('30', 'Parking', '19.99', '0', '10');
+-- Room 11
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('31', 'WI-FI', '12.99', '0', '11');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('32', 'Breakfast', '8.99', '0', '11');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('33', 'Parking', '19.99', '0', '11');
+-- Room 12
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('34', 'WI-FI', '12.99', '0', '12');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('35', 'Breakfast', '8.99', '0', '12');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('36', 'Parking', '19.99', '0', '12');
+-- Room 13
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('37', 'WI-FI', '12.99', '0', '13');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('38', 'Breakfast', '8.99', '0', '13');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('39', 'Parking', '19.99', '0', '13');
+-- Room 14
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('40', 'WI-FI', '12.99', '0', '14');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('41', 'Breakfast', '8.99', '0', '14');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('42', 'Parking', '19.99', '0', '14');
+-- Room 15
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('43', 'WI-FI', '12.99', '0', '15');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('44', 'Breakfast', '8.99', '0', '15');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('45', 'Parking', '19.99', '0', '15');
+-- Room 16
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('46', 'WI-FI', '12.99', '0', '16');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('47', 'Breakfast', '8.99', '0', '16');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('48', 'Parking', '19.99', '0', '16');
+-- Room 17
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('49', 'WI-FI', '12.99', '0', '17');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('50', 'Breakfast', '8.99', '0', '17');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('51', 'Parking', '19.99', '0', '17');
+-- Room 18
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('52', 'WI-FI', '12.99', '0', '18');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('53', 'Breakfast', '8.99', '0', '18');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('54', 'Parking', '19.99', '0', '18');
+-- Room 19
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('55', 'WI-FI', '12.99', '0', '19');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('56', 'Breakfast', '8.99', '0', '19');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('57', 'Parking', '19.99', '0', '19');
+-- Room 20
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('58', 'WI-FI', '12.99', '0', '20');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('59', 'Breakfast', '8.99', '0', '20');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('60', 'Parking', '19.99', '0', '20');
+-- Room 21
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('61', 'WI-FI', '12.99', '0', '21');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('62', 'Breakfast', '8.99', '0', '21');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('63', 'Parking', '19.99', '0', '21');
+-- Room 22
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('64', 'WI-FI', '12.99', '0', '22');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('65', 'Breakfast', '8.99', '0', '22');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('66', 'Parking', '19.99', '0', '22');
+-- Room 23
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('67', 'WI-FI', '12.99', '0', '23');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('68', 'Breakfast', '8.99', '0', '23');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('69', 'Parking', '19.99', '0', '23');
+-- Room 24
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('70', 'WI-FI', '12.99', '0', '24');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('71', 'Breakfast', '8.99', '0', '24');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('72', 'Parking', '19.99', '0', '24');
+-- Room 25
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('73', 'WI-FI', '12.99', '0', '25');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('74', 'Breakfast', '8.99', '0', '25');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('75', 'Parking', '19.99', '0', '25');
+-- Room 26
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('76', 'WI-FI', '12.99', '0', '26');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('77', 'Breakfast', '8.99', '0', '26');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('78', 'Parking', '19.99', '0', '26');
+-- Room 27
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('79', 'WI-FI', '12.99', '0', '27');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('80', 'Breakfast', '8.99', '0', '27');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('81', 'Parking', '19.99', '0', '27');
+-- Room 28
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('82', 'WI-FI', '12.99', '0', '28');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('83', 'Breakfast', '8.99', '0', '28');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('84', 'Parking', '19.99', '0', '28');
+-- Room 29
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('85', 'WI-FI', '12.99', '0', '29');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('86', 'Breakfast', '8.99', '0', '29');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('87', 'Parking', '19.99', '0', '29');
+-- Room 30
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('88', 'WI-FI', '12.99', '0', '30');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('89', 'Breakfast', '8.99', '0', '30');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('90', 'Parking', '19.99', '0', '30');
+-- Room 31
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('91', 'WI-FI', '12.99', '0', '31');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('92', 'Breakfast', '8.99', '0', '31');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('93', 'Parking', '19.99', '0', '31');
+-- Room 32
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('94', 'WI-FI', '12.99', '0', '32');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('95', 'Breakfast', '8.99', '0', '32');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('96', 'Parking', '19.99', '0', '32');
+-- Room 33
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('97', 'WI-FI', '12.99', '0', '33');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('98', 'Breakfast', '8.99', '0', '33');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('99', 'Parking', '19.99', '0', '33');
+-- Room 34
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('100', 'WI-FI', '12.99', '0', '34');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('101', 'Breakfast', '8.99', '0', '34');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('102', 'Parking', '19.99', '0', '34');
+-- Room 35
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('103', 'WI-FI', '12.99', '0', '35');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('104', 'Breakfast', '8.99', '0', '35');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('105', 'Parking', '19.99', '0', '35');
+-- Room 36
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('106', 'WI-FI', '12.99', '0', '36');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('107', 'Breakfast', '8.99', '0', '36');
+INSERT INTO `amenities`.`categories` (`amenities_id`, `name`, 'cost', 'chargeFrequency', 'room_id') VALUES ('108', 'Parking', '19.99', '0', '36');
