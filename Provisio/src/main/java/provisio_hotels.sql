d39d8960937f2a7daa5d3a240f616e1905b2dcc8
ghostman5658 <88981912+ghostman5658@users.noreply.github.com>
2022-08-28 17:16:01 -0400

    Week 3 push

--- Provisio/src/main/java/provisio_hotels.sql
+++ Provisio/src/main/java/provisio_hotels.sql
@@ -1,11 +1,11 @@
 -- Hotel 1
-INSERT INTO 'provisio'.'hotels' ('HotelId', 'HotelName', 'Address', 'PostCode', 'City', 'Country', 'NumberOfRooms', 'PhoneNumber', 'Attraction1', 'Attraction2', 'Attraction3');
-VALUES ('1', 'Hotel One', '123 1st St', '68135', 'Omaha', 'United States', '1', '402-555-5555', 'Henry Doorly Zoo', 'College World Series', 'Nebraska Football Game');
+INSERT INTO 'provisio'.'hotel' ('HotelId', 'HotelName', 'Address', 'PostCode', 'City', 'Country', 'NumberOfRooms', 'PhoneNumber', 'Attraction1', 'Attraction2', 'Attraction3');
+VALUES (1, 'Hotel One', '123 1st St', 68135, 'Omaha', 'United States', 1, '402-555-5555', 'Henry Doorly Zoo', 'College World Series', 'Nebraska Football Game');
 
 -- Hotel 2
-INSERT INTO 'provisio'.'hotels' ('HotelId', 'HotelName', 'Address', 'PostCode', 'City', 'Country', 'NumberOfRooms', 'PhoneNumber', 'Attraction1', 'Attraction2', 'Attraction3');
-VALUES ('2', 'Hotel Two', '456 2nd St', '80014', 'Denver', 'United States', '1', '720-555-5555', 'Rocky Mountain National Park', 'Denver Botanic Gardens', 'Coors Field');
+INSERT INTO 'provisio'.'hotel' ('HotelId', 'HotelName', 'Address', 'PostCode', 'City', 'Country', 'NumberOfRooms', 'PhoneNumber', 'Attraction1', 'Attraction2', 'Attraction3');
+VALUES (2, 'Hotel Two', '456 2nd St', 80014, 'Denver', 'United States', 1, '720-555-5555', 'Rocky Mountain National Park', 'Denver Botanic Gardens', 'Coors Field');
 
 -- Hotel 3
-INSERT INTO 'provisio'.'hotels' ('HotelId', 'HotelName', 'Address', 'PostCode', 'City', 'Country', 'NumberOfRooms', 'PhoneNumber', 'Attraction1', 'Attraction2', 'Attraction3');
-VALUES ('3', 'Hotel Three', '789 3rd St', '90001', 'Los Angeles', 'United States', '1', '310-555-5555', 'The Getty Center ', 'Santa Monica Pier and Beach', 'Universal Studios Hollywood');
+INSERT INTO 'provisio'.'hotel' ('HotelId', 'HotelName', 'Address', 'PostCode', 'City', 'Country', 'NumberOfRooms', 'PhoneNumber', 'Attraction1', 'Attraction2', 'Attraction3');
+VALUES (3, 'Hotel Three', '789 3rd St', 90001, 'Los Angeles', 'United States', 1, '310-555-5555', 'The Getty Center ', 'Santa Monica Pier and Beach', 'Universal Studios Hollywood');
--- Provisio/src/main/java/provisio_rooms.sql
+++ Provisio/src/main/java/provisio_rooms.sql
@@ -1,41 +1,41 @@
 -- Hotel 1
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('1', 'Queen Room', 'queen', '1', '125.00', '150', '1')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (1, 'Queen Room', 'queen', 1, 125.00, 150, 1);
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('2', 'Double Room', 'double full', '2', '110.00', '150', '1')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (2, 'Double Room', 'double full', 2, 110.00, 150, 1);
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('3', 'King Room', 'king', '3', '165.00', '150', '1')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (3, 'King Room', 'king', 3, 165.00, 150, 1);
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('4', 'Double Queen Room', 'double queen', '4', '150.00', '150', '1')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (4, 'Double Queen Room', 'double queen', 4, 150.00, 150, 1);
 
 -- Hotel 2
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('4', 'Queen Room', 'queen', '1', '125.00', '150', '2')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (5, 'Queen Room', 'queen', 1, 125.00, 150, 2);
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('6', 'Double Room', 'double full', '2', '110.00', '150', '2')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (6, 'Double Room', 'double full', 2, 110.00, 150, 2);
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('7', 'King Room', 'king', '3', '165.00', '150', '2')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (7, 'King Room', 'king', 3, 165.00, 150, 2);
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('8', 'Double Queen Room', 'double queen', '4', '150.00', '150', '2')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (8, 'Double Queen Room', 'double queen', 4, 150.00, 150, 2);
 
 -- Hotel 3
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('9', 'Queen Room', 'queen', '1', '125.00', '150', '3')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (9, 'Queen Room', 'queen', 1, 125.00, 150, 3);
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('10', 'Double Room', 'double full', '2', '110.00', '150', '3')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (10, 'Double Room', 'double full', 2, 110.00, 150, 3);
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('11', 'King Room', 'king', '3', '165.00', '150', '3')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (11, 'King Room', 'king', 3, 165.00, 150, 3);
 
-INSERT INTO 'provisio'.'hotels'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId')
-VALUES ('12', 'Double Queen Room', 'double queen', '4', '150.00', '150', '3')
+INSERT INTO 'provisio'.'room'('RoomId', 'Name', 'Size', 'NumberOfGuests', 'BasePrice', 'Points', 'HotelId');
+VALUES (12, 'Double Queen Room', 'double queen', 4, 150.00, 150, 3);
