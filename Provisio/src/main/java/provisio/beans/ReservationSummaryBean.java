3d41313e6a4bd8c4ce8a7550ff4c693e795d4c05
cheher21 <cheher@my365.bellevue.edu>
2022-09-10 16:30:44 -0500

    Added code to Bean (Testing)

--- Provisio/src/main/java/provisio/beans/ReservationSummaryBean.java
+++ Provisio/src/main/java/provisio/beans/ReservationSummaryBean.java
@@ -2,6 +2,55 @@ package provisio.beans;
 
 import java.sql.Connection;
 
-public class ReservationSummaryBean {
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
+public class ReservationSummaryBean {
+	public String setCity(String city) {
+		String cityLocation = "";
+		
+		
+		
+		if(city == "Omaha")
+		{
+			cityLocation = "Omaha";
+		}
+		else if(city == "Denver")
+		{
+			cityLocation = "Denver";
+		}
+		else
+		{
+			cityLocation = "Los Angeles";
+		}
+		
+		return cityLocation;
+	}
+	
+	public Date dayLength(Date checkin, Date checkout) {
+		Date totalDays = null;
+		
+		
+		
+		return totalDays;
+	}
 }
+
+// ****Archive Code****
+//if(city == "Omaha") {
+//cityLocation = "Omaha";
+//} else if(city == "Denver") {
+//cityLocation = "Denver";
+//} else if(city == "LosAngeles") {
+//cityLocation = "Los Angeles";
+//}
+
+//if(room.toString() == "Queen") {
+//out.print("One Queen Bed");
+//} else if(room.toString() == "Double") {
+//out.print("Two Twin Beds");
+//} else if(room.toString() == "King") {
+//out.print("One King Bed");
+//} else {
+//out.print("Two Queen Beds");
+//}
